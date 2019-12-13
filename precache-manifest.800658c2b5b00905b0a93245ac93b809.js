c3b460c74b0a2aeca6a072cec49ac99fe30bb896
Chris Brody <chrisbrody@codifyacademy.com>
2019-12-13 09:16:02 -0500

    Updates

---
+++ precache-manifest.800658c2b5b00905b0a93245ac93b809.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "295c2385f2537ce76d451f908163dd6b",
+    "url": "/project-react-js-task-manager/index.html"
+  },
+  {
+    "revision": "60b62000efd5973af274",
+    "url": "/project-react-js-task-manager/static/css/main.8eb78f1c.chunk.css"
+  },
+  {
+    "revision": "738126796c5a0a9e278d",
+    "url": "/project-react-js-task-manager/static/js/2.d36b4e92.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/project-react-js-task-manager/static/js/2.d36b4e92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60b62000efd5973af274",
+    "url": "/project-react-js-task-manager/static/js/main.33a5b40d.chunk.js"
+  },
+  {
+    "revision": "613e2c116cece756b137",
+    "url": "/project-react-js-task-manager/static/js/runtime-main.1d04e538.js"
+  }
+]);
